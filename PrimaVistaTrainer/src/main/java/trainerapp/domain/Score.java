0d42a64ae45ad84d07927e30bb78a18d68833607
Antti Hartikka <antti.hartikka@protonmail.com>
2021-03-30 18:21:26 +0300

    bravura lisätty, score-luokka aloitettu

--- PrimaVistaTrainer/src/main/java/trainerapp/domain/Score.java
+++ PrimaVistaTrainer/src/main/java/trainerapp/domain/Score.java
@@ -1,4 +1,96 @@
 package trainerapp.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Random;
+
 public class Score {
+
+    private int noteBoundLow = 38;
+    private int noteBoundHigh = 83;
+    private int[] notes;
+    private int[] scale;
+    private boolean[] isFlat;
+    private boolean[] isNormalized;
+    private boolean[] isSharp;
+    private int[] degrees;
+
+    public Score() {
+        scale = new int[]{0,2,4,5,7,9,11};
+        degrees = new int[]{0,0,1,1,2,3,3,4,4,5,5,6};
+    }
+
+    public void generate(int length) {
+        Random r = new Random();
+        notes = new int[length];
+        int octave;
+        while(notes[0] < noteBoundLow || notes[0] > noteBoundHigh) {
+            octave = ((noteBoundLow / 12) + r.nextInt((noteBoundHigh - noteBoundLow) / 12)) * 12;
+            notes[0] = octave + scale[0];
+        }
+        for (int i = 1; i < length; i++) {
+            while (notes[i] < noteBoundLow || notes[i] > noteBoundHigh) {
+                float a = r.nextFloat();
+                int prevNote = notes[i - 1] % 12;
+                int prevDeg = degrees[prevNote];
+                if (a < 0.2) {
+                    notes[i] = prevNote;
+                } else if (a < 0.3) {
+                    notes[i] = getNoteByDegreeChange(prevDeg, -1);
+                } else if (a < 0.4) {
+                    notes[i] = getNoteByDegreeChange(prevDeg, 1);
+                } else if (a < 0.45) {
+                    notes[i] = getNoteByDegreeChange(prevDeg, -2);
+                } else if (a < 0.5) {
+                    notes[i] = getNoteByDegreeChange(prevDeg, 2);
+                } else if (a < 0.55) {
+                    notes[i] = getNoteByDegreeChange(prevDeg, -3);
+                } else if (a < 0.6) {
+                    notes[i] = getNoteByDegreeChange(prevDeg, 3);
+                } else if (a < 0.7) {
+                    notes[i] = getNoteByDegreeChange(prevDeg, -4);
+                } else if (a < 0.8) {
+                    notes[i] = getNoteByDegreeChange(prevDeg, 4);
+                } else if (a < 0.85) {
+                    notes[i] = getNoteByDegreeChange(prevDeg, -5);
+                } else if (a < 0.9) {
+                    notes[i] = getNoteByDegreeChange(prevDeg, 5);
+                } else if (a < 0.95){
+                    notes[i] = getNoteByDegreeChange(prevDeg, -6);
+                } else {
+                    notes[i] = getNoteByDegreeChange(prevDeg, 6);
+                }
+                int newOctave = (2 + r.nextInt(4)) * 12;
+                notes[i] += newOctave;
+                if (notes[i] < noteBoundLow || notes[i] > noteBoundHigh) {
+                    continue;
+                }
+                octave = newOctave;
+            }
+        }
+    }
+
+    private int getNoteByDegreeChange(int prev, int change) {
+        return scale[(prev + 12 + change) % 7];
+    }
+
+    public int[] getNotes() {
+        return notes;
+    }
+
+    public int[] getScale() {
+        return scale;
+    }
+
+    public int getDegrees(int note) {
+        return degrees[note % 12];
+    }
+
+    public static void main(String[] args) {
+        Score s = new Score();
+        s.generate(32);
+        System.out.println(Arrays.toString(s.notes));
+    }
+
 }
--- PrimaVistaTrainer/src/main/java/trainerapp/ui/UserInterface.java
+++ PrimaVistaTrainer/src/main/java/trainerapp/ui/UserInterface.java
@@ -1,28 +1,48 @@
 package trainerapp.ui;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.text.Font;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
+import trainerapp.domain.Score;
+
+import java.io.File;
+import java.util.Map;
 
 public class UserInterface extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Font leland = Font.loadFont("file:resources/fonts/Leland.otf", 72);
+        Font bravura = Font.loadFont("file:resources/fonts/BravuraText.otf", 100);
         Font edwin = Font.loadFont("file:resources/fonts/Edwin-Roman.otf", 36);
 
         BorderPane pane = new BorderPane();
-        Text hello = new Text("Hello World!");
-        hello.setFont(edwin);
-        pane.setCenter(hello);
-        Scene scene = new Scene(pane, 1000, 600);
-        primaryStage.setScene(scene);
+        Canvas canvas = new Canvas(1000, 700);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.BLACK);
+        gc.setFont(bravura);
 
+        BorderPane asettelu = new BorderPane();
+        asettelu.setCenter(canvas);
+
+        Score score = new Score();
+        score.generate(12);
+        ScoreDrawer drawer = new ScoreDrawer(score, gc, 30, 300);
+        gc.strokeLine(30, 300, 300, 300);
+        drawer.draw();
+
+        Scene scene = new Scene(asettelu);
+        primaryStage.setScene(scene);
         primaryStage.setTitle("Prima vista trainer");
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
